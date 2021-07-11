4cdc0739432c5040a725472461a77668a9ab7a04
plantain <dlstkaaktclt@naver.com>
2021-07-12 00:26:43 +0900

    backup

---
+++ phenu.py
@@ -0,0 +1,14 @@
+# Importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import warnings
+import os
+#!pip install tensorflow==2.1.0
+#!pip install keras==2.3.1
+%matplotlib inline
+
+#pd.set_option("display.max_rows", None,"display.max_columns", None)
+warnings.simplefilter(action='ignore')
+plt.style.use('seaborn')
